735f44a1926c71a32264c56208147cb8677e9384
Adarsh Mishra <adarshmishra.2507@gmail.com>
2026-06-01 09:54:10 +0530

    Question 1

---
+++ Week_1/Day_1/Ques_1.java
@@ -0,0 +1,24 @@
+package Summer_Assignment_2401920100030.Week_1.Day_1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ques_1 {
+    class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int need = target - nums[i];
+
+            if (map.containsKey(need)) {
+                return new int[] { map.get(need), i };
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[] {};
+    }
+}
+}
