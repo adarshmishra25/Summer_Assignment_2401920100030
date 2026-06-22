bc2d081b98c8b9254e07e5f1040dc8e6e8645311
Adarsh Mishra <adarshmishra.2507@gmail.com>
2026-06-22 12:39:57 +0530

    Week 4 Day 1 Questions

---
+++ Week_4/Day_1/Ques_2.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null ) return null;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int len = queue.size();
+            for(int i = 0 ; i < len ; i++ ){
+                TreeNode node = queue.poll();
+                swap(node);
+                if(node.left != null) queue.offer(node.left);
+                if(node.right != null) queue.offer(node.right);
+            }
+        }
+        return root;
+    }
+    private void swap(TreeNode node){
+        TreeNode temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+    }
+}
